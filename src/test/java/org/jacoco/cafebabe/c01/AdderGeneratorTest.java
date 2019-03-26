ff6b2604403426d5b0195efdce2121cb2869e51b
Evgeny Mandrikov <mandrikov@gmail.com>
2019-03-26 01:20:57 +0100

    Use MethodHandle to improve readability

--- src/test/java/org/jacoco/cafebabe/c01/AdderGeneratorTest.java
+++ src/test/java/org/jacoco/cafebabe/c01/AdderGeneratorTest.java
@@ -2,6 +2,9 @@ package org.jacoco.cafebabe.c01;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 import org.jacoco.cafebabe.test.MemoryClassLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,11 +23,13 @@ class AdderGeneratorTest {
 	}
 
 	@Test
-	void add_should_add_two_integers() throws Exception {
+	void add_should_add_two_integers() throws Throwable {
 		cl.add(AdderGenerator.create());
 
 		Class<?> clazz = cl.loadClass("Adder");
-		int result = (int) clazz.getMethod("add", Integer.TYPE, Integer.TYPE).invoke(null, 13, 29);
+		int result = (int) MethodHandles.lookup()
+				.findStatic(clazz, "add", MethodType.methodType(int.class, int.class, int.class)) //
+				.invoke(13, 29);
 
 		assertEquals(42, result);
 	}
